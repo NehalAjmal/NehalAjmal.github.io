0e5340c76572677259aa4c337e089dc9858a2988
Nehal Ajmal <2k24.cs1k.2410488@gmail.com>
2025-02-20 22:55:57 +0530

    Update index.tsx

--- src/components/@projects/Project/index.tsx
+++ src/components/@projects/Project/index.tsx
@@ -1,4 +1,4 @@
-'use cleint'
+'use client'
 
 import { createPortal } from 'react-dom'
 import { useRef, useEffect, memo } from 'react'
@@ -6,41 +6,40 @@ import { useGSAP } from '@gsap/react'
 import { useLenis } from '@studio-freight/react-lenis'
 import dynamic from 'next/dynamic'
 import Image from 'next/image'
-
 import { X } from 'lucide-react'
+
+// Dynamically import the ProjectContent component
 const ProjectContent = dynamic(() => import('./Content'), { ssr: false })
 import { ProjectAnimation } from './Animations'
-
 import { Project as ProjectType } from '@typess/types'
 
 interface ProjectProps {
   project: ProjectType
-  closeProject: () => void;
+  closeProject: () => void
 }
 
-function Project ({ closeProject, project }: ProjectProps) {
+function Project({ closeProject, project }: ProjectProps) {
   const lenis = useLenis()
-
   const containerRef = useRef<HTMLDivElement>(null)
 
+  // Pause Lenis when the modal is open
   useEffect(() => {
     if (lenis) {
       lenis.stop()
     }
-
     return () => {
       if (lenis) {
         lenis.start()
       }
     }
-  }, [ lenis ])
+  }, [lenis])
 
+  // Apply GSAP animation to the modal
   useGSAP(() => {
     if (!containerRef.current) return
-    
     ProjectAnimation(containerRef.current)
   }, {
-    scope: containerRef
+    scope: containerRef,
   })
 
   return createPortal(
@@ -48,16 +47,17 @@ function Project ({ closeProject, project }: ProjectProps) {
       ref={containerRef}
       className="fixed inset-2 z-50 flex items-center justify-between gap-2 overflow-hidden rounded-xl bg-100 p-2 shadow-shadow sm:inset-3 sm:p-3 md:inset-4 md:p-4 lg:inset-5 lg:p-5"
     >
+      {/* Close button */}
       <span
         onClick={closeProject}
         className="absolute right-2.5 top-2.5 max-h-7 max-w-7 cursor-pointer transition-all duration-300 ease-out hover:opacity-65 hover:scale-125"
       >
-        <X className="size-full text-error"/>
+        <X className="size-full text-error" />
       </span>
 
+      {/* Modal content */}
       <span className="flex size-full flex-col items-center justify-between gap-4 overflow-scroll p-2 sm:gap-6 sm:p-3 md:flex-row md:gap-8 md:p-4 lg:gap-10 lg:p-5">
         <ProjectContent project={project} />
-
         <div className="relative flex flex-1 select-none items-center justify-center overflow-hidden rounded-xl object-cover shadow-shadow">
           <Image
             src={`/projects/${project.image}`}
@@ -68,9 +68,7 @@ function Project ({ closeProject, project }: ProjectProps) {
             height={500}
           />
         </div>
-
       </span>
-      
     </div>,
     document.body
   )
